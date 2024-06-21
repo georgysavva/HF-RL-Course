3f61c610bfc2bfd494f18986429e6c90cb21760e
Georgy Savva <georgy.savva@nyu.edu>
2024-06-21 16:27:30 -0400

    init

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt install python-opengl
+sudo apt install ffmpeg
+sudo apt install xvfb
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+
+pip install -e ./ml-agents/ml-agents-envs
+pip install -e ./ml-agents/ml-agents
