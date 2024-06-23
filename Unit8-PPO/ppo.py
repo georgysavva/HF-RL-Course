b68e15df317e49a6d091fafc41486392cd46e6e2
Georgy Savva <georgy.savva@nyu.edu>
2024-06-23 14:46:48 -0400

    use poetry and define requirements

---
+++ Unit8-PPO/ppo.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.categorical import Categorical
+from torch.utils.tensorboard import SummaryWriter
